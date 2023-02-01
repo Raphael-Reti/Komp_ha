554a912abd9aa25d33c32cb71464e0d9b2e99316
Arnold Chis <119848325+Mineix@users.noreply.github.com>
2023-02-01 11:23:56 +0100

    Update 1

--- Kompha.java
+++ Kompha.java
@@ -1,6 +1,16 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
+
 public class Kom_Aufgabe {
+	private static void generateBox(String msg) {    //Methode erstellen um eine statisch erzeugte Box zu generieren um ausgabe box der variable anzupassen
+		int lenght = msg.length();      // länge soll der nachhricht angepasst werden
+		int width = lenght - 2;         // breite ebenfalls nur 2 charackter kleiner
+		PrintStream out =  System.out;         
+		out.println("\n╭─"+"━".repeat(width) + "─╮" ); // eigentliche befehl mit 
+		out.println("|"+msg+"|" );
+		out.println("╰─"+"━".repeat(width) + "─╯" );
+	}
 	static void wandleBinInDez() {
 
 	}
@@ -11,76 +21,102 @@ public class Kom_Aufgabe {
 		String bin = "";
 		int dezZahl, i = 364, count = 0;
 		char menu, dezimal, ITrechner = 0;
-		
-		
+		boolean open, start = true;
+
 		int anzahlVerschiebung = 0;
 		int dezZahl1 = 0;
 		int restWert = 0;
 
 		String hex = Integer.toHexString(i);
-		System.out.println("Was Möchten sie Rechnen," 
-		        + " \n ╭─━━━━━━━━━━━━━━━━━━━━━━━━━━─╮ "
-				+ " \n |Dez --> Bin|Bitte D Eigeben,|  (⌐■_■)--︻╦╤─ - - - - - -      ̿ ̿ ̿ ̿'\\̵͇̿̿\\з=(◕_◕) "
-				+ " \n |Bin --> Dez|Bitte B Eigeben,|" + " \n |Dez --> Hex|Bitte H Eigeben.|"
-				+ " \n ╰─━━━━━━━━━━━━━━━━━━━━━━━━━━─╯");
-		ITrechner = Kon.readChar();
-		switch (ITrechner) {
-		case 'D': {
+		while (start) { 
+			generateBox("Bitte true eingeben zum Starten");
+			open = Kon.readBoolean();
+			if(open = true) {
+				generateBox("Was möchten sie Rechnen");
+			System.out.println("" 
+					+ " ╭─━━━━━━━━━━━━━━━━━━━━━━━━━━─╮ "
+					+ " \n |Dez --> Bin|Bitte D Eigeben,|"  // (⌐■_■)--︻╦╤─ - - - - - -      ̿ ̿ ̿ ̿'\\̵͇̿̿\\з=(◕_◕) "
+					+ " \n |Bin --> Dez|Bitte B Eigeben,|" 
+					+ " \n |Dez --> Hex|Bitte H Eigeben.|"
+					+ " \n ╰─━━━━━━━━━━━━━━━━━━━━━━━━━━─╯");
+			ITrechner = Kon.readChar();}
+			switch (ITrechner) {
+			case 'D': {
+				System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━─╮");
+				System.out.println(" |Bitte DezZahl Eingeben:  |");
+				System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━─╯");
+				
+				dezZahl = Kon.readInt();
+				while (dezZahl != 0) {
+					if (dezZahl % 2 == 0) {
+						bin = "0" + bin;
+					} else {
+						bin = "1" + bin;
+					}
 
-			System.out.println("Bitte Dezimalzahl eingeben: ");
-			dezZahl = Kon.readInt();
-			while (dezZahl != 0) {
-				if (dezZahl % 2 == 0) {
-					bin = "0" + bin;
+					dezZahl = dezZahl / 2;
+					count++;
+					if (count == 4) {
+						bin = " " + bin;
+						count = 0;
+					}
+				}
+				
+			generateBox("Ihre BinZahl Lautet: "+ bin );
+				
+
+				break;
+			}
+			case 'B': {
+				Scanner eingabe = new Scanner(System.in);
+				System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━─╮");
+				System.out.println(" |Bitte Binzahl eingeben!  |");
+				System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━─╯");
+				int binZahl = eingabe.nextInt();
+				while (binZahl != 0) {
+					restWert = binZahl % 10;
+					dezZahl1 = dezZahl1 + (int) (restWert * (Math.pow(2, anzahlVerschiebung)));
+					binZahl = binZahl / 10;
+					anzahlVerschiebung = anzahlVerschiebung + 1;
+				}
+				if (dezZahl1 > 10) {
+					System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━─╮");
+					System.out.println(" |" + "Ihre DezZahl lautet: " + dezZahl1 + " " + " |");
+					System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━─╯");
+
 				} else {
-					bin = "1" + bin;
+					System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━─╮");
+					System.out.println(" |" + "Ihre DezZahl lautet: " + " " + dezZahl1 + " " + " |");
+					System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━─╯");
+
 				}
 
-				dezZahl = dezZahl / 2;
-				count++;
-				if (count == 4) {
-					bin = " " + bin;
-					count = 0;
-				}
+				System.out.println();
+		
+				break;
+
 			}
 
-			System.out.println("Ihre Binäre Zahl lautet: " + bin);
-			break;
-		}
-		case 'B': {
-			Scanner eingabe = new Scanner(System.in);
-			System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━─╮");
-			System.out.println(" |Bitte Dualzahl eingeben! |");
-			System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━─╯");
-			int binZahl = eingabe.nextInt();
-			while (binZahl != 0) {
-				restWert = binZahl % 10;
-				dezZahl1 = dezZahl1 + (int) (restWert * (Math.pow(2, anzahlVerschiebung)));
-				binZahl = binZahl / 10;
-				anzahlVerschiebung = anzahlVerschiebung + 1;
+			case 'H': {
+				System.out.println("Bitte Dezimalzahl eingeben: ");
+				dezZahl = Kon.readInt();
+				System.out.println("Hexwert: 0x" + hex);
+				Integer in = Integer.parseInt(hex, 16);
+				System.out.println("Dezimalwert: " + in);
+				
+				break;
+
 			}
-			System.out.println(" ╭─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╮");
-			System.out.println(" |" + "Ihre DezZahl lautet"+ dezZahl1 + "|");
-			System.out.println(" ╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯");
 			
-			System.out.println();
-			break;
-		}
+			case 'X': {
+				System.out.println("Programm Wird Beendet!! ");
+				start = false;
+			}
+			default: {
+				System.out.println("Bye,Bye");
+			}
 
-		case 'H': {
-			System.out.println("Bitte Dezimalzahl eingeben: ");
-			dezZahl = Kon.readInt();
-			System.out.println("Hexwert: 0x" + hex);
-			Integer in = Integer.parseInt(hex, 16);
-			System.out.println("Dezimalwert: " + in);
-
-			break;
+			}
 		}
-		default: {
-			System.out.println("Fehler, Error 69");
-		}
-
-		}
-
 	}
 }
